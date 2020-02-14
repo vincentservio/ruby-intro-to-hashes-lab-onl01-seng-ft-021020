ebdbd98e259e102c2640ac83b88f839a1cbd59a9
Vincent Servio <vincentservio@gmail.com>
2020-02-14 06:32:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,8 +20,8 @@ def monopoly_with_second_tier
     {:pieces => 4
      :names => { 
      :rent_in_dollars => { }
-    
-    
+      }
+     }
 	 }
 	
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
