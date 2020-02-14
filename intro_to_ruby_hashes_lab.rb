de6cc6f50634dc30169789fdaae2e147a8a7adc9
Vincent Servio <vincentservio@gmail.com>
2020-02-14 06:57:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,9 @@ def monopoly_with_fourth_tier
     :railroads => {
     :pieces => 4,
      :names => {
-       :reading_railroad => {},
+       :reading_railroad => {
+         :mortgage_value =>
+       },
        :pennsylvania_railroad => {},
        :b_and_o_railroad => {},
        :shortline => {}
