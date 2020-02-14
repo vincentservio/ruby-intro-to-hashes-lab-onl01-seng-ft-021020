feca633ab5074471c2e22684333fa8ae6bbe4ecb
Vincent Servio <vincentservio@gmail.com>
2020-02-14 05:59:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 
 def monopoly
   monopoly = { 
-    :railroads => { :pieces => 4} 
+    :railroads => { 
     
   }
 # binding.pry
@@ -16,7 +16,7 @@ end
 
 def monopoly_with_second_tier
 	 monopoly = { 
-    :railroads => {} 
+    :railroads => {:pieces => 4} }
     
   }
 	
