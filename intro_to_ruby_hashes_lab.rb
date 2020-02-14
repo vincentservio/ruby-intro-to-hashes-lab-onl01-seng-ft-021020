8a281a5a0b9b0789f4e5ec0d4387bd1740c4a18c
Vincent Servio <vincentservio@gmail.com>
2020-02-14 07:03:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,7 +59,7 @@ def monopoly_with_fourth_tier
        :pennsylvania_railroad => {
          "mortgage_value" => "$200"},
        :b_and_o_railroad => {
-         "mortgage_value" => "$100"
+         "mortgage_value" => "$400"
        },
        :shortline => {"mortgage_value" => "$800"}
      },
