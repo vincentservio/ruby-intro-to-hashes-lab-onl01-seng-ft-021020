e34ed10315335fd5a9a606be51074f1fbc286183
Vincent Servio <vincentservio@gmail.com>
2020-02-14 07:02:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,7 +61,7 @@ def monopoly_with_fourth_tier
        :b_and_o_railroad => {
          "mortgage_value" => "$100"
        },
-       :shortline => {mortgage_value => "$800"}
+       :shortline => {"mortgage_value" => "$800"}
      },
      :rent_in_dollars => {
        :one_piece_owned => 25,
